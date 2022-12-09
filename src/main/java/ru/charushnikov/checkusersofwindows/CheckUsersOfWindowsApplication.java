ccd314b996fc41b7555bcba21b349e4c8c821d98
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-12-09 22:50:51 +0300

    После запуска Spring, Windows открывает ссылку на localhost

--- src/main/java/ru/charushnikov/checkusersofwindows/CheckUsersOfWindowsApplication.java
+++ src/main/java/ru/charushnikov/checkusersofwindows/CheckUsersOfWindowsApplication.java
@@ -3,11 +3,14 @@ package ru.charushnikov.checkusersofwindows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class CheckUsersOfWindowsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(CheckUsersOfWindowsApplication.class, args);
+        Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler localhost:8080");
     }
 
 }
